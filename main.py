ff8321bdae233a8ca1e06c3800425fc305c1eee5
IX-0 <igorbaltarejo05@gmail.com>
2023-12-05 11:37:15 +0000

    neggus

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as ttk 
+import requests as rqst
+
+root = ttk.Tk()
+
+button1 = ttk.Button(root,text="Teste",fg="black",bg="white")
+
+
+def main():
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+    
