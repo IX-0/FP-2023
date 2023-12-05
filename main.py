5129b03f9de7abb66f41bbf4342aebc082c08fe4
Tiago-0li-veira <tiago.oliveira1@ua.pt>
2023-12-05 12:54:28 +0000

    hrthr

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ import requests as rqst
 root = ttk.Tk()
 
 button1 = ttk.Button(root,text="Teste",fg="black",bg="white")
-
-
-
-        
-
-def main():
+def categ_dict ():
     with open("categories.txt","r") as file:
         categ_dict={}
         for line in file:
             categories = line.strip().split(".")
-            if len(categories)!=1:
+            if len(categories)==1:
                 categ_dict[categories[0]] = categ_dict.get(categories[0],[])
                 categ_dict[categories[0]].append(categories[1])
             else:
-                continue    
-    x,y = input("Enter coordinates split by coma: ").strip().split(",")
+                continue
+def countlen():
+    with open("categories.txt","r") as file:
+        print (max([len(line.strip().split(".")) for line in file]))    
+        
+
+def main():
+    countlen() 
+    """ x,y = input("Enter coordinates split by coma: ").strip().split(",")
     coordinates = (float(x),float(y))
     radius = float(input("Enter radius: "))
-    categs = input("Enter categories split by coma: ")
+    categs = input("Enter categories split by coma: ") """
     
     
     
