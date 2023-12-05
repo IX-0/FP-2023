c652a8b75775daa791958dfb82ace0658851b991
Tiago-0li-veira <tiago.oliveira1@ua.pt>
2023-12-05 12:11:24 +0000

    dict categories

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import tkinter as ttk
+import tkinter as ttk 
+import requests as rqst
 
 root = ttk.Tk()
 
-def main():
-    root.mainloop()
+button1 = ttk.Button(root,text="Teste",fg="black",bg="white")
 
+
+
+        
+
+def main():
+    with open("categories.txt","r") as file:
+        categ_dict={}
+        for line in file:
+            categories = line.strip().split(".")
+            if len(categories)!=1:
+                categ_dict[categories[0]] = categ_dict.get(categories[0],[])
+                categ_dict[categories[0]].append(categories[1])
+            else:
+                continue    
+    root.mainloop()
+    print(categ_dict)
 if __name__ == "__main__":
     main()
+    
