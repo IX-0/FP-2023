04934f091fe40e57766210d74a39788130579ca6
IX-0 <igorbaltarejo05@gmail.com>
2023-12-05 12:37:21 +0000

    dread

--- API.py
+++ API.py
@@ -1,1 +1,18 @@
 import requests as rqst
+
+def get(baseURL,params={}):
+    newURL = baseURL + "?"
+    for k,v in params.items():
+        newURL += f"&{k}={v}"
+
+    request = rqst.get(newURL)
+    return request.json()
+
+request = {
+"apiKey":"d9d7d63a741949f6913b267674ca0f16",
+"categories":"accommodation,activity",
+"limit":"10",
+"bias":"proximity:-0.07071648508463113,51.50848194136378"
+}
+
+print(get("https://api.geoapify.com/v2/places",request))
