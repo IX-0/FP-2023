ee760b9571edb37a3856f35d23f1c39fccbc0691
Tiago-0li-veira <tiago.oliveira1@ua.pt>
2023-12-06 02:19:58 +0000

    dicionário era inútil, ent fiz um set ≽^•⩊•^≼

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 import tkinter as ttk 
 import requests as rqst
 
-root = ttk.Tk()
+#root = ttk.Tk()
+#button1 = ttk.Button(root,text="Teste",fg="black",bg="white")
 
-button1 = ttk.Button(root,text="Teste",fg="black",bg="white")
-def categ_dict ():
-    with open("categories.txt","r") as file:
-        categ_dict={}
-        for line in file:
-            categories = line.strip().split(".")
-            if len(categories)==1:
-                categ_dict[categories[0]] = categ_dict.get(categories[0],[])
-                categ_dict[categories[0]].append(categories[1])
-            else:
-                continue
+            
 def countlen():
-    with open("categories.txt","r") as file:
-        print (max([len(line.strip().split(".")) for line in file]))    
+    with open(r'C:\Users\Utilizador\Desktop\Tiago\GitRepositories\FP-2023\categories.txt') as file:
+        print(max([len(line.strip().split('.')) for line in file]))
         
+        
+def all_categs():
+    with open(r'C:\Users\Utilizador\Desktop\Tiago\GitRepositories\FP-2023\categories.txt') as file:
+        all_categs={categ for line in file for categ in line.strip().split('.')}
+    return all_categs    
+    
+def make_dict():
+    with open(r'C:\Users\Utilizador\Desktop\Tiago\GitRepositories\FP-2023\categories.txt') as file:
+        return {k:v for k,v in [(line[:-1],line.strip().split('.')) for line in file]}  
+      
+def filter_categs(categs):
+    categ_set = all_categs()
+    categ_lst = categs.strip().split(',')
+    error_msg = ""
+    validated_categs=[]
+    for categ in categ_lst:
+        if categ in categ_set:
+            validated_categs.append(categ)
+        else:
+            error_msg += "'{}' is not a valid category.\n".format(categ)
+    print(error_msg, end="")
+    return ','.join(validated_categs)           
 
 def main():
-    countlen() 
-    """ x,y = input("Enter coordinates split by coma: ").strip().split(",")
-    coordinates = (float(x),float(y))
-    radius = float(input("Enter radius: "))
-    categs = input("Enter categories split by coma: ") """
+    countlen()
+    categ_set = all_categs() 
     
+    """x, y = input("Enter coordinates split by coma: ").strip().split(",")
+    coordinates = (float(x), float(y))
+    radius = float(input("Enter radius: "))
+    categs = input("Enter categories split by coma: ")"""
+    
+    
+    print(make_dict())
     
     
 if __name__ == "__main__":
