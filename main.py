7375dbbc09a2b1f4849fffcf0edb95b5d07180fa
Tiago-0li-veira <tiago.oliveira1@ua.pt>
2023-12-06 14:13:14 +0000

    filter

--- main.py
+++ main.py
@@ -14,10 +14,7 @@ def all_categs():
     with open(r'C:\Users\Utilizador\Desktop\Tiago\GitRepositories\FP-2023\categories.txt') as file:
         all_categs={categ for line in file for categ in line.strip().split('.')}
     return all_categs    
-    
-def make_dict():
-    with open(r'C:\Users\Utilizador\Desktop\Tiago\GitRepositories\FP-2023\categories.txt') as file:
-        return {k:v for k,v in [(line[:-1],line.strip().split('.')) for line in file]}  
+
       
 def filter_categs(categs):
     categ_set = all_categs()
@@ -29,12 +26,30 @@ def filter_categs(categs):
             validated_categs.append(categ)
         else:
             error_msg += "'{}' is not a valid category.\n".format(categ)
-    print(error_msg, end="")
-    return ','.join(validated_categs)           
+    print(error_msg, end="") 
+    with open(r'C:\Users\Utilizador\Desktop\Tiago\GitRepositories\FP-2023\categories.txt') as file:
+        dict = {k:v for k,v in [(line[:-1],line.strip().split('.')) for line in file]}            
+    lst=[]
+    for categ in validated_categs:
+        for k,v in dict.items():
+            if categ == k or categ in v:
+                lst.append(k)
+    return(','.join(lst))
+
+
+def get_categs(categs):
+    with open(r'C:\Users\Utilizador\Desktop\Tiago\GitRepositories\FP-2023\categories.txt') as file:
+        dict = {k:v for k,v in [(line[:-1],line.strip().split('.')) for line in file]}            
+    lst=[]
+    for categ in categs:
+        for k,v in dict.items():
+            if categ == k or categ in v:
+                lst.append(k)
+    print(','.join(lst))
 
 def main():
     countlen()
-    categ_set = all_categs() 
+
     
     """x, y = input("Enter coordinates split by coma: ").strip().split(",")
     coordinates = (float(x), float(y))
@@ -42,7 +57,7 @@ def main():
     categs = input("Enter categories split by coma: ")"""
     
     
-    print(make_dict())
+    print(filter_categs('accommodation,tourism,poop,golly'))
     
     
 if __name__ == "__main__":
