4cba35f8a57db26b6cc7dc0b0939909434286140
Tiago-0li-veira <tiago.oliveira1@ua.pt>
2023-12-07 14:35:04 +0000

    a

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 from API import *
 from main import *
+
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("800x500")
+root.title("Test")
+
+label = tk.Label(root, text="Hello world!", font=("Arial",25))
