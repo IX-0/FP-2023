7c17542f47bb38e0d1d98bb035a2f9125c9dcd24
Tiago-0li-veira <tiago.oliveira1@ua.pt>
2023-12-11 01:32:58 +0000

    já funciona tudo, só que tá feio

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 import tkinter as tk 
+from tkinter import ttk
 from API import *
 
 
-def addPlaces(frame:tk.Frame):
-
-    """Function that adds location objects to a frame"""
-
-    #DEBUG
-    data = get("https://api.geoapify.com/v2/places",params={'apiKey':'d9d7d63a741949f6913b267674ca0f16','categories':'accommodation','limit':'20','bias':'proximity:-0.07071648508463113,51.50848194136378'})
-
-    places = convertRequest(data)
-    #DEBUG END
-
-    for place in places:
-        placeFrame = tk.LabelFrame(frame, text=place['name'])
-
-        tk.Label(placeFrame, text='Distance: {}'.format(place['distance']))
-        tk.Label(placeFrame, text='Categories: {}'.format(place['categories']))
-
-        for element in placeFrame.winfo_children():
-            element.pack()
-
-        placeFrame.pack()
 
 
 def clearPlaces(frame:tk.Frame):
@@ -29,22 +10,23 @@ def clearPlaces(frame:tk.Frame):
         place.destroy()
         
 
+
 def set_all_categs(fileDir):
     """Creates a set with all the possible categories or sub-categories in file"""
-    with open(fileDir) as f:
+    with open(fileDir,'r') as f:
         return {categ for line in f for categ in line.strip().split('.')} 
    
    
 def dict_all_categs(fileDir):
     """Creates a dictionary with all the categories in the file as keys and the correspondent sub-categories as values."""
-    with open(fileDir) as f:
+    with open(fileDir,'r') as f:
         return {k:v for k,v in [(line.strip() ,line.strip().split('.')) for line in f]}
 
       
 def filter_categs(s:str, fileDir):
     """Removes any categories not existent in file and returns the following pattern: <category>,<categorie>,(...),<categorie>"""
     categ_set = set_all_categs(fileDir)
-    categ_lst = [categ.strip() for categ in s.split(',')]
+    categ_lst = [categ.strip() for categ in s.strip().split(',')]
 
     validated_categs = [categ for categ in categ_lst if categ in categ_set]
 
@@ -110,7 +92,48 @@ def main():
         width= 50
     )
 
-    searchButton.configure(command= lambda: addPlaces(frame=placesFrame))
+    def make_params():
+    
+        lon = lonEntry.get()
+        lat = latEntry.get()
+        categs = categoryEntry.get()
+        categs = filter_categs(categs,'categories.txt')
+        params = {
+            'apiKey':'d9d7d63a741949f6913b267674ca0f16',
+            'categories':categs,
+            'bias':'proximity:{},{}'.format(lon,lat)
+        }
+        
+        return params
+    
+    def addPlaces(frame:tk.Frame):
+
+        """Function that adds location objects to a frame"""
+
+        #DEBUG
+        data = get("https://api.geoapify.com/v2/places",make_params())
+
+        places = convertRequest(data)
+        #DEBUG END
+        
+        
+        total_dist=0
+        n=0
+        for place in places:
+            if place.get('name',False):
+                placeFrame = tk.LabelFrame(frame, text=place['name'])
+
+                tk.Label(placeFrame, text='Distance: {}'.format(place['distance']))
+                tk.Label(placeFrame, text='Categories: {}'.format(place['categories']))
+                total_dist+=place['distance']
+                n+=1
+                for element in placeFrame.winfo_children():
+                    element.pack()
+
+                placeFrame.pack()
+        avg_dist = total_dist/n
+
+    searchButton.configure(command= lambda: addPlaces(frame=placesFrame,))
     clearButton.configure(command= lambda: clearPlaces(frame=placesFrame))
     #Placement
     searchButton.grid(column=2,row=0,rowspan=2)
@@ -126,6 +149,8 @@ def main():
     lonEntry.grid(column=1,row=2)
 
     placesFrame.grid(column=0,row=3,columnspan=3)
+    
+    
 
     root.mainloop()
 
